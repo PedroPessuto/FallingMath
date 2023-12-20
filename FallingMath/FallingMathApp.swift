67795b67fb5ff7e8fa4831144730a350b79a762a
Pedro Pessuto <pedro.pessuto@outlook.com>
2023-12-20 17:32:05 -0300

    Initial Commit

---
+++ FallingMath/FallingMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  FallingMathApp.swift
+//  FallingMath
+//
+//  Created by Pedro Pessuto on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FallingMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
