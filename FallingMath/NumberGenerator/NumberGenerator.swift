59fe055bf22a8d5a69811c8c6ef8d34cd12f1e23
RaphaelIniesta <raphael_iniesta@yahoo.com>
2024-01-17 15:00:48 -0300

    Atualizar a geração de operações

--- FallingMath/NumberGenerator/NumberGenerator.swift
+++ FallingMath/NumberGenerator/NumberGenerator.swift
@@ -11,7 +11,7 @@ func generator() -> Array<Int> {
     var number: Int = 0
     var numberArray: [Int] = []
     
-    for _ in 1...5 {
+    for _ in 1...10 {
         number = Int.random(in: 1..<51)
         numberArray.append(number)
     }
@@ -24,34 +24,43 @@ func operationGenerator() {
     let array: [Int] = generator()
     
     // Variáveis de todas as operações separadas
-    var soma: [Int] = []
-    var sub: [Int] = []
-    var mult: [Int] = []
-    var div: [Float] = []
+    var soma: [String] = []
+    var sub: [String] = []
+    var mult: [String] = []
+    var div: [String] = []
     
     // Variáveis de transformação de Int -> Float
     var array1: Float = 0.0
     var array2: Float = 0.0
+    
+    // Array que salva todas as operações
+    var operations: [Array<String>] = []
     
     // Imprime o Array inicial que será usado para fazer as operações
     print(array)
     for i in 0...array.count - 1 {
         for j in 0...array.count - 1 {
             if(i != j) {
-                soma.append(array[i] + array[j])
-                sub.append(array[i] - array[j])
-                mult.append(array[i] * array[j])
+                soma.append(String(array[i] + array[j]))
+                sub.append(String(array[i] - array[j]))
+                mult.append(String(array[i] * array[j]))
                 array1 = Float(array[i])
                 array2 = Float(array[j])
-                div.append(array1 / array2)
+                let num = array1 / array2
+                
+                // Formata o Float para uma casa decimal
+                let formatted = String(format: "%.1f", num)
+                div.append(formatted)
             }
         }
     }
     
-    print("Soma: \(soma)")
-    print("Subtração: \(sub)")
-    print("Multiplicação: \(mult)")
-    print("Divisão \(div)")
+    operations.append(soma)
+    operations.append(sub)
+    operations.append(mult)
+    operations.append(div)
+    
+    print(operations)
     
     
 //    return [1,2,3,4]
